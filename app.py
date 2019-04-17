fab21582c72cd81aebd173f6db4d641ce416f5bd
someikou <zuming.jiang@thscom.co.jp>
2019-04-17 16:27:28 +0900

    ''

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from utils.parseUtil import *
+import re
+from urllib.parse import urlparse,unquote
 
 opts = webdriver.ChromeOptions()
 opts.binary_location = 'C:\\Program Files (x86)\\Google\Chrome Beta\\Application\\chrome.exe'
@@ -11,36 +13,81 @@ driver = webdriver.Chrome(chrome_options = opts)
 driver.implicitly_wait(10)
 driver.set_window_size(360, 800)
 
-data = {
-    'loginUrl':'https://grp02.id.rakuten.co.jp/rms/nid/vc?__event=login&service_id=p11',
+testData = {
+    'loginUrl':'',
     'username':'',
     'password':'',
     'itemList':'',
-    'item':"https://affiliate.rakuten.co.jp/link/pc/item?me_id=1193666&item_id=10044582&me_url=https%3A%2F%2Fitem.rakuten.co.jp%2Fvehicle%2F33000008%2F&me_img_src=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fvehicle%2Fcabinet%2Fgios%2F2019%2F33000008.jpg&goods_name=%E3%83%AD%E3%83%BC%E3%83%89%E3%83%AC%E3%83%BC%E3%82%B5%E3%83%BC+2019+GIOS+%E3%82%B8%E3%82%AA%E3%82%B9+FURBO+%E3%83%95%E3%83%AB%E3%83%9C+%E3%82%B8%E3%82%AA%E3%82%B9%E3%83%96%E3%83%AB%E3%83%BC&mitem_flg=1&price=95040&tax_flg=0&postage_flg=0&change_flg=0"
+    'item':''
 }
-driver.get(data['loginUrl'])
-username = driver.find_element_by_name('u')
-password = driver.find_element_by_name('p')
-username.send_keys(data['username'])
-password.send_keys(data['password'])
-driver.find_element_by_name('submit').send_keys(Keys.ENTER)
 
-element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, "sitem"))
-)
-driver.get(data['item'])
-codebox = driver.find_element_by_id('codebox').get_attribute('value')
-data = parseCodeBox(codebox)
+def login ():
+    driver.get(testData['loginUrl'])
+    username = driver.find_element_by_name('u')
+    password = driver.find_element_by_name('p')
+    username.send_keys(testData['username'])
+    password.send_keys(testData['password'])
+    driver.find_element_by_name('submit').send_keys(Keys.ENTER)
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "sitem"))
+    )
 
-imgContainer = driver.find_element_by_id('alt_imgs_container')
-imgList = imgContainer.find_elements_by_class_name('p-1')
-imgUrlList = []
-for imgUrl in imgList
-    imgUrlList.append(imgUrl.get_attribute('src'))
-data['imgUrlList'] = imgUrlList
-print(data['titleUrl'])
-print(data['imgUrlList'][0])
+def jumpToItemList () :
+    driver.get(testData['itemList'])
+    itemsList = driver.find_elements_by_css_selector('p[class="mb-2 mb-lg-3"] a')
+    href = itemsList[0].get_attribute('href')
+    print(href)
+    targetList = []
+    for item in itemsList:
+        itemTargetUrl = item.get_attribute('href')
+        itemId = re.findall('item_id=[a-zA-Z0-9_]{1,}',itemTargetUrl)[0].replace('item_id=','')
+        price = re.findall('price=[a-zA-Z0-9_]{1,}',itemTargetUrl)[0].replace('price=','')
+        title = unquote(re.findall('goods_name=[a-zA-Z0-9_\+\-.%]{1,}',itemTargetUrl)[0].replace('goods_name=',''))
+        itemSaleUrl = unquote(re.findall('me_url=[a-zA-Z0-9_.\-%]{1,}',itemTargetUrl)[0].replace('me_url=',''))
+        shopName = re.findall('co.jp/[a-zA-Z0-9_.\-%]{1,}',itemSaleUrl)[0].replace('co.jp/','')
+        data = {
+            'itemTargetUrl':itemTargetUrl,
+            'itemId':itemId,
+            'title':title,
+            'price':price,
+            'shopName':shopName,
+            'itemSaleUrl':itemSaleUrl
+        }
+        targetList.append(data)
 
+    for target in targetList:
+        jumpToItemPage(target)
 
+def jumpToItemPage(target):
+    # driver.get(testData['item'])
+    driver.get(target['itemTargetUrl'])
+    codebox = driver.find_element_by_id('codebox').get_attribute('value')
+    data = parseCodeBox(codebox)
+    target['titleUrl'] = data['titleUrl']
+    target['btnUrl'] = data['btnUrl']
+    imgContainer = driver.find_element_by_id('alt_imgs_container')
+    imgList = imgContainer.find_elements_by_class_name('p-1')
+    imgUrlList = []
+    for imgUrl in imgList:
+        imgUrlList.append(imgUrl.get_attribute('src'))
+    target['imgUrlList'] = imgUrlList
+    # target['shopName'] = driver.find_element_by_id('shop_name').text
+    target['shopUrl'] = driver.find_element_by_id('jump_url').get_attribute('href')
+    target['shopMsg'] = driver.find_element_by_id('shop_blurb').text
+    print(target['itemId'])
+    print(target['title'])
+    print(target['price'])
+    print(target['shopName'])
+    print(target['itemSaleUrl'])
+    print(target['titleUrl'])
+    print(target['btnUrl'])
+    print(target['imgUrlList'][0])
+    print(target['shopName'])
+    print(target['shopUrl'])
+    print(target['shopMsg'])
+
+login()
+# jumpToItemPage()
+jumpToItemList()
 
 
